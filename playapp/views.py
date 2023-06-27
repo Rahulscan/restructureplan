100d198a0f4b9493cb02a25825d4f440510d8ae6
rahulscan <rahul.sharma@scanova.io>
2023-06-27 16:57:36 +0530

    big changes

--- playapp/views.py
+++ playapp/views.py
@@ -7,15 +7,3 @@ from .serializers import PlanSerializer
 class QuotaPlanAPIView(generics.RetrieveAPIView):
     serializer_class = PlanSerializer
     queryset = Plan.objects.all()
-
-# class QuotaValueAPIView(generics.RetrieveAPIView):
-#
-#     def get(self, request, **kwargs):
-#         try:
-#             pk = kwargs.get('pk')
-#             plan = Plan.objects.get(pk=pk)
-#             quotas = plan.all_quotas
-#             serializer = QuotaValueSerializer(quotas, many=True)
-#             return Response(serializer.data)
-#         except Plan.DoesNotExist:
-#             return Response({"error": "Plan not found."}, status=404)
